fb03be2cdf39e3f722b1a14e60b56fd2b5074816
mehmetfaahem <mehmetfaahem@gmail.com>
2025-04-06 03:00:19 +0600

    vercel build error fix

--- app/product/[id]/generateStaticParams.ts
+++ app/product/[id]/generateStaticParams.ts
@@ -1,13 +1,14 @@
 // This file handles static site generation parameters
 
+import { products } from "@/app/api/products/route";
 import { Product } from "@/lib/types";
 
 export async function generateStaticParams() {
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/api/products`);
-      if (!res.ok) return [];
+    //   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/api/products`);
+    //   if (!res.ok) return [];
   
-      const products = await res.json();
+    //   const products = await res.json();
       return products.map((product: Product) => ({ id: product.id }));
     } catch (error) {
       console.error('Failed to fetch products:', error);
